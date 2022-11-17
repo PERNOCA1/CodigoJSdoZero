9ecddf489538e31c5f8d149e7a3ae7c7ba637e0e
Your Name <you@example.com>
2022-11-17 09:01:14 -0300

    Operacoes com variaveis

---
+++ 2-Operacoes/index.js
@@ -0,0 +1,41 @@
+// OPERAÇÕES MATEMATICA
+var resultado;
+var x = 2;
+var y = 5;
+
+//soma
+resultado = x + y;
+
+console.log('Soma: ', resultado)
+
+//subtracao
+resultado = x - y;
+
+console.log('Subtração: ', resultado)
+
+//multiplicacao
+resultado = x * y;
+
+console.log('Multiplicacão: ', resultado)
+
+//divisao
+resultado = x / y;
+
+console.log('Divisão: ', resultado)
+
+// calculo do modulo (resto da divisao)
+resultado = x % y;
+console.log('Modulo: ', resultado)
+
+// calculo avancado
+var peso = 80;
+var altura = 1.7;
+
+var imc = (peso / (altura * altura))
+console.log('IMC = ', imc)
+
+// soma de strings
+var primeiroNome = 'Gabriel';
+var segundoNome = 'Bianchi';
+
+console.log('Nome Completo : ', primeiroNome + '' + segundoNome)

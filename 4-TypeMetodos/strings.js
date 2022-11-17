02842897015b6d4535296ea6fae32e4cceabfd60
Your Name <you@example.com>
2022-11-17 12:01:34 -0300

    Métodos de cada Type

---
+++ 4-TypeMetodos/strings.js
@@ -0,0 +1,19 @@
+/* Métodos de Strings
+1 - Lenght
+2 - indexOf
+3 - Search
+4 - Slice | Substring
+5 - Replace
+6 - UpperCase | LoweCase
+7 - Trin
+8 - ChatAt
+*/
+var text = 'Bem vindo ao SevenCoders'
+
+// 1 - Lenght
+
+console.log('Lenght (tamanho): ',text.length)
+
+// 2 - indexOf
+
+console.log('Indice do: ', text.indexOf)
